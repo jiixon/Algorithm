d861241c054f0aa14b692207bbcefd3d2428e71d
JIWON IN <jr0107380@gmail.com>
2023-08-24 00:33:28 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,8 @@
+def solution(binomial):
+    b_list = binomial.split(' ')
+    if b_list[1] == '+':
+        return int(b_list[0]) + int(b_list[2])
+    elif b_list[1] == '-':
+        return int(b_list[0]) - int(b_list[2])
+    else:
+        return int(b_list[0]) * int(b_list[2])
