823dd2d306007e9a85cb2a792e7caba0d09874ec
JIWON IN <jr0107380@gmail.com>
2023-09-08 21:06:50 +0900

    [unrated] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,10 @@
+def solution(order):
+    answer=0
+    for i in order:
+        if "cafelatte" in i:
+            answer+=5000
+        else:
+            answer+=4500
+    return answer
+            
+            
