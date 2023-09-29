44a3febeb6f4136c9c84516d040db464edfa5c8b
JIWON IN <jr0107380@gmail.com>
2023-09-30 02:39:04 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int num = x;
+        while(num>=1){
+            sum += num%10;
+            num /= 10;
+        }
+        if(x%sum ==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
