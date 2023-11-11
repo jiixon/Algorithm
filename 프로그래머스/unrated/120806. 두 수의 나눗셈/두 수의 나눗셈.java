f07ee7c5c7877cb0f74917226416bf0b005d1b77
JIWON IN <jr0107380@gmail.com>
2023-11-11 20:25:39 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double answer=(num1*1.0)/num2;
+        
+        int result =(int)(answer*1000);
+        return result;
+    }
+}
