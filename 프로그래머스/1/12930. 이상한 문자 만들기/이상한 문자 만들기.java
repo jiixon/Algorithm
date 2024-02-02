840b861cc2272fcb14bb2b00f564a604c73c7798
JIWON IN <jr0107380@gmail.com>
2024-02-02 22:38:22 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.64 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] array = s.split(" ",-1);
+        System.out.println(Arrays.toString(array));
+        
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array[i].length(); j++){
+                if(j%2==0) sb.append(Character.toUpperCase(array[i].charAt(j)));
+                else sb.append(Character.toLowerCase(array[i].charAt(j)));
+            }
+            sb.append(" ");
+        }
+        sb.deleteCharAt(s.length());
+        System.out.println(sb.toString());
+
+        return sb.toString();
+    }
+}
