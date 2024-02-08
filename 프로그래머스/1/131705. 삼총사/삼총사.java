37f59bcc6bea47547941599bccfe43039260e5df
JIWON IN <jr0107380@gmail.com>
2024-02-09 00:40:47 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -2,19 +2,14 @@ class Solution {
     public int solution(int[] number) {
         int result = 0;
         int sum=0;
-        for(int i=0; i<number.length; i++){
-            sum=0;
-            for(int j=i+1; j<number.length; j++){
-                sum=0;
+        for(int i=0; i<number.length-2; i++){
+            for(int j=i+1; j<number.length-1; j++){
                 for(int k=j+1; k<number.length; k++){
-                    sum=0;
                     sum+=number[i]+number[j]+number[k];
-                    System.out.println(number[i]+ "+"+ number[j] +"+"+ number[k]);
-                    
-                    System.out.println(sum);
                     if(sum==0){
                         result+=1;
                     }
+                    sum=0;
                 }
             }
         }
