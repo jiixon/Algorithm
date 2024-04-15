3c7f0cac4b991562b0b84b6d6d2aaae3bccb20d7
JIWON IN <jr0107380@gmail.com>
2024-04-16 03:39:25 +0900

    [Silver V] Title: 덩치, Time: 156 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,43 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bf.readLine());
+        String[][] input = new String[num][2];
+        for (int i = 0; i < num; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            input[i][0] = st.nextToken();
+            input[i][1] = st.nextToken();
+        }
+
+        int[][] count = new int[num][2];
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j <2; j++){
+                count[i][j] = 0;
+            }
+        }
+        int[] result = new int[num];
+
+        for(int i=0; i<num; i++){
+            for(int j=0; j< num; j++){
+                if(i!=j){
+                    if(Integer.parseInt(input[i][0]) < Integer.parseInt(input[j][0]) &&
+                            (Integer.parseInt(input[i][1]) < Integer.parseInt(input[j][1]))){
+                        result[i]++;
+                    }
+                }
+
+            }
+        }
+        for (int i = 0; i < num; i++) {
+            System.out.print((result[i] + 1) + " ");
+        }
+    }
+}
