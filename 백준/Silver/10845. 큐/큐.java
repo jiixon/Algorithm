bd4511c207326004620f6a8dab0bc37ffa211a8c
JIWON IN <jr0107380@gmail.com>
2024-04-25 15:39:41 +0900

    [Silver IV] Title: 큐, Time: 196 ms, Memory: 18596 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -20,23 +20,19 @@ public class Main {
                 int num = Integer.parseInt(value.nextToken());
                 q.offer(num);
             }else if(command.equals("pop")){
-                Integer item = q.poll();
-                if(item == null){
-                    sb.append(-1).append("\n");
-                }else sb.append(item).append("\n");
+                if(q.isEmpty()) sb.append(-1).append("\n");
+                else sb.append(q.poll()).append("\n");
             }else if(command.equals("size")){
                 sb.append(q.size()).append("\n");
             }else if(command.equals("empty")){
                 if(q.isEmpty()) sb.append(1).append("\n");
                 else sb.append(0).append("\n");
             }else if(command.equals("front")){
-                Integer peek = q.peek();
-                if(peek == null) sb.append(-1).append("\n");
-                else sb.append(peek).append("\n");
+                if(q.isEmpty()) sb.append(-1).append("\n");
+                else sb.append(q.peek()).append("\n");
             }else if(command.equals("back")){
-                Integer peekLast = q.peekLast();
-                if(peekLast == null) sb.append(-1).append("\n");
-                else sb.append(peekLast).append("\n");
+                if(q.isEmpty()) sb.append(-1).append("\n");
+                else sb.append(q.peekLast()).append("\n");
             }
         }
         System.out.println(sb);
