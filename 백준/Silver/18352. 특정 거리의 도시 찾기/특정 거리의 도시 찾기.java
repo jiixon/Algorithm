bf16e06dbbfe3b3bb99dfbe23859dd99586b70f5
JIWON IN <jr0107380@gmail.com>
2024-06-28 15:27:07 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1176 ms, Memory: 267300 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,6 +7,29 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static int[] distance;
+    public static ArrayList<ArrayList<Integer>> graph;
+
+    public static void bfs(int start) {
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        //시작점 큐에 넣기
+        queue.offer(start);
+        distance[start] = 0;
+
+        while (!queue.isEmpty()) {
+            Integer curNode = queue.poll();
+
+            //인접노드 확인
+            for (int i = 0; i < graph.get(curNode).size(); i++) {
+                Integer nxtNode = graph.get(curNode).get(i);
+                if (distance[nxtNode] == -1){
+                    distance[nxtNode] = distance[curNode]+1; //간선 모두 1이므로 최단거리 갱신
+                    queue.offer(nxtNode);
+                }
+            }
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,43 +38,31 @@ public class Main {
         int k = Integer.parseInt(st.nextToken()); //거리정보(도달해야하는 거리)
         int x = Integer.parseInt(st.nextToken()); //출발 도시
 
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i<n+1; i++){
+        graph = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for(int i = 0; i<m; i++){
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             graph.get(a).add(b);
         }
-        LinkedList<Integer> queue = new LinkedList<>();
-        int[] distance = new int[n + 1];
-        Arrays.fill(distance, -1); //-1로 초기화
-        distance[x] = 0;
-        queue.add(x);
 
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            for (int next : graph.get(current)) {
-                if (distance[next] == -1) { // 아직 방문하지 않은 도시라면
-                    distance[next] = distance[current] + 1;
-                    queue.add(next);
-                }
-            }
-        }
-        // 거리 k인 모든 도시를 출력
+        distance = new int[n + 1];
+        Arrays.fill(distance, -1); //최단거리 배열 모두 -1로 초기화
+
+        bfs(x);
+        
         boolean found = false;
-        for (int i = 1; i <= n; i++) {
-            if (distance[i] == k) {
+        for(int i =1; i<n+1; i++){
+            if(distance[i] == k){
                 System.out.println(i);
                 found = true;
             }
         }
 
-        if (!found) {
-            System.out.println(-1);
-        }
+        if(!found) System.out.println(-1);
     }
 }
