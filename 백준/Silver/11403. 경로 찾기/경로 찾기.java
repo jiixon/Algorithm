b5d6b7bc21ae585908a0682ac4a684f97066a069
JIWON IN <jr0107380@gmail.com>
2024-06-29 21:01:38 +0900

    [Silver I] Title: 경로 찾기, Time: 316 ms, Memory: 19384 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,39 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] graph = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (graph[j][i] == 1 && graph[i][k] == 1) {
+                        graph[j][k] = 1;
+                    }
+                }
+            }
+        }
+
+        //출력
+        for(int i = 0; i<N; i++){
+            for(int j = 0; j<N; j++){
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
