6d2053fbf3c08bd6b3ffd9e36fd23775a4bd5def
JIWON IN <jr0107380@gmail.com>
2025-03-22 23:37:33 +0900

    [level 2] Title: 피로도, Time: 3.77 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,21 +1,6 @@
-import java.util.*;
 class Solution {
-    static int maxCount;
     public int solution(int k, int[][] dungeons) {
-        boolean[] visited = new boolean[dungeons.length];
-        maxCount = 0;
-        dfs(k, visited, dungeons, maxCount);
-        return maxCount;
-    }
-    
-    public void dfs(int k, boolean[] visited, int[][] dungeons, int count){
-        maxCount = Math.max(count, maxCount);
-        for(int i = 0; i<dungeons.length; i++){
-            if(!visited[i]&& dungeons[i][0] <= k){
-                visited[i] = true;
-                dfs(k - dungeons[i][1], visited, dungeons, count+1);
-                visited[i] = false;
-            }
-        }
+        int answer = -1;
+        return answer;
     }
 }
