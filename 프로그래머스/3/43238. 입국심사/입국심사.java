861307ddcd676c6b805814c6e03516d695a8bdf2
JIWON IN <jr0107380@gmail.com>
2025-03-30 23:28:43 +0900

    [level 3] Title: 입국심사, Time: 108.93 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        long answer = 0;
+        long left = 1;
+        long right = (long)times[times.length-1]*n; //최장시간
+        long mid;
+        while (left <= right) {
+            mid = (left + right)/2; //중간값
+            
+            long sum = 0; //중간값동안 탐색할 수 있는 총시간
+            for(int time : times){
+                sum += mid / time;
+                if(sum > n) break;
+            }
+            
+            if(sum >= n){
+                answer = mid;
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+            
+        }
+        return answer;
+    }
+}
