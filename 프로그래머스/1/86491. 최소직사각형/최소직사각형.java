0723d9cfb272e5230efbb5ae6b7b7d685682e447
JIWON IN <jr0107380@gmail.com>
2025-06-02 21:08:35 +0900

    [level 1] Title: 최소직사각형, Time: 3.80 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,17 +1,14 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
-        for(int[] array : sizes){
-            Arrays.sort(array);
+        for(int[] arr : sizes){
+            Arrays.sort(arr);
         }
         int w = sizes[0][0];
         int h = sizes[0][1];
-        int answer = 0;
-        
-        for(int[] array : sizes){
-            if(w<array[0]) w=array[0];
-            if(h<array[1]) h=array[1];
-        
+        for(int[] arr : sizes){
+            if(w<arr[0]) w = arr[0];
+            if(h<arr[1]) h = arr[1];
         }
         return w*h;
     }
