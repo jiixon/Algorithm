69dd9e4e8fd5106bf85be539058f472a4c5ca303
JIWON IN <jr0107380@gmail.com>
2025-06-13 22:50:33 +0900

    [level 3] Title: N으로 표현, Time: 35.80 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,38 +1,33 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-        int answer = 0;
-        Set<Integer>[] dp = new HashSet[9];
-        if(N == number) {
-            return 1;
-        }
-        for(int i = 1; i<=8; i++) {
+        Set<Integer>[] dp = new Set[9];
+        if(N == number) return 1;
+        for(int i = 1; i<=8; i++){
             dp[i] = new HashSet<>();
             
-            int num = 0; 
+            int num = 0;
+            //N을 i번 사용해서 만들 수 있는 숫자 채우기
             for(int n = 0; n<i; n++){
                 num = 10*num + N;
+                dp[i].add(num); //N, NN, NNN
             }
-            dp[i].add(num);
             
-            for(int j = 1; j<=i-1; j++) {
-                int k = i-j;
-                for(int x : dp[j]) {
+            for(int j = 1; j<=i-1; j++ ){
+                int k = i - j;
+                //i = 3이면, dp[1]과 dp[2] 연산
+                for(int x : dp[j]){
                     for(int y : dp[k]){
                         dp[i].add(x+y);
                         dp[i].add(x-y);
                         dp[i].add(x*y);
                         if(y!=0) dp[i].add(x/y);
                     }
-                   
                 }
-                 
+
             }
-            if (dp[i].contains(number)) {
-                return i;
-            }
+            if(dp[i].contains(number)) return i;
         }
-        
         return -1;
     }
 }
