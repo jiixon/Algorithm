acccab260746e5b971cb72f9a6fac826943715fe
JIWON IN <jr0107380@gmail.com>
2025-07-02 17:56:27 +0900

    [level 3] Title: 가장 먼 노드, Time: 29.21 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,53 +2,45 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
         List<List<Integer>> graph = new ArrayList<>();
-        
-        for(int i = 0 ; i<=n; i++){
+        for(int i = 0; i<=n; i++){
             graph.add(new ArrayList<>());
         }
         
-        //edge[i] = {u, v}
         for(int[] e : edge){
             int u = e[0];
             int v = e[1];
-            //양방향 그래프
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
         
         int[] distance = new int[n+1];
         LinkedList<Integer> queue = new LinkedList<>();
+        distance[1] = 1;
         queue.offer(1);
-        distance[1] = 1; //시작 노드
         
-        //BFS
         while(!queue.isEmpty()){
-            int target = queue.poll();
+            int current = queue.poll();
             
-            //target과 인접한 노드 방문처리
-            for(int i = 0; i<graph.get(target).size(); i++){
-                int next = graph.get(target).get(i);
-                //아직 방문하지 않은 노드라면
+            for(int i = 0; i<graph.get(current).size(); i++){
+                int next = graph.get(current).get(i);
+                
                 if(distance[next] == 0){
-                    distance[next] = distance[target] + 1; //거리 갱신
+                    distance[next] = distance[current] + 1;
                     queue.offer(next);
                 }
             }
-            
         }
         
-        //가장 먼 거리 찾기
-        int maxDistance = 0; 
+        int maxDistance = 0;
         for(int d : distance){
-            maxDistance = Math.max(maxDistance, d);
+            maxDistance = Math.max(d, maxDistance);
         }
         
-        //가장 먼 거리의 노드 수 세기
         int count = 0;
-        for(int d :distance){
-            if(d == maxDistance) count++;
+        for(int d : distance){
+            if(maxDistance == d) count++;
         }
-               
+        
         return count;
     }
 }
