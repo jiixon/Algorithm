b7a5491bc24781258cf4f7dc39a77c327892558e
JIWON IN <jr0107380@gmail.com>
2025-09-20 04:19:53 +0900

    [level 3] Title: 가장 먼 노드, Time: 26.58 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
 
 ### 성능 요약
 
-메모리: 121 MB, 시간: 29.21 ms
+메모리: 119 MB, 시간: 26.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 02일 17:56:22
+2025년 09월 20일 04:19:50
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,43 +2,50 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
         List<List<Integer>> graph = new ArrayList<>();
+        
         for(int i = 0; i<=n; i++){
             graph.add(new ArrayList<>());
         }
         
-        for(int[] e : edge){
+        //edge[i] = {u, v};
+        for(int[] e : edge) {
             int u = e[0];
             int v = e[1];
+            //양방향 그래프
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
         
         int[] distance = new int[n+1];
         LinkedList<Integer> queue = new LinkedList<>();
-        distance[1] = 1;
+        //시작 노드
         queue.offer(1);
+        distance[1] = 1;
         
+        //bfs
         while(!queue.isEmpty()){
-            int current = queue.poll();
+            int target = queue.poll();
             
-            for(int i = 0; i<graph.get(current).size(); i++){
-                int next = graph.get(current).get(i);
-                
-                if(distance[next] == 0){
-                    distance[next] = distance[current] + 1;
+            //target과 인접한 노드 방문
+            for(int i = 0; i<graph.get(target).size(); i++){
+                int next = graph.get(target).get(i); //인접 노드
+                if(distance[next] == 0){ //방문안한 노드라면,
+                    distance[next] = distance[target] + 1; //거리 갱신
                     queue.offer(next);
                 }
             }
         }
         
+        //가장 먼 거리 찾기
         int maxDistance = 0;
         for(int d : distance){
-            maxDistance = Math.max(d, maxDistance);
+            if(d>maxDistance) maxDistance = d;
         }
         
+        //가장 먼 거리의 노드 찾기
         int count = 0;
         for(int d : distance){
-            if(maxDistance == d) count++;
+            if(d == maxDistance) count++;
         }
         
         return count;
