5757929596d1ee6521bb96d2cafcded212322e4c
JIWON IN <jr0107380@gmail.com>
2025-09-29 03:23:00 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,19 +1,21 @@
 class Solution {
     public int solution(int n, int[][] computers) {
-        int answer = 0; 
-        boolean[] visited = new boolean[n];
+        int count = 0;
+        boolean[] visited = new boolean[n]; //방문노드 확인배열
         for(int i = 0; i<n; i++){
-            if(!visited[i]){
+            if(visited[i] == false){
                 dfs(i, computers, visited);
-                answer++;
+                count++;
             }
+            
         }
-        return answer;
+        return count;
     }
-    private void dfs(int node, int[][] computers, boolean[] visited){
-        visited[node] = true;
+    
+    private void dfs(int node, int[][] computers, boolean[] visited) {
+        visited[node] = true; //방문처리
         for(int i = 0; i<computers.length; i++){
-            if(computers[node][i] == 1 && !visited[i]){
+            if(computers[node][i] == 1 && node!=i && !visited[i]){
                 dfs(i, computers, visited);
             }
         }
