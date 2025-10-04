8072366f77e7bf54c9cfda70d9af2a7e9148c1c9
JIWON IN <jr0107380@gmail.com>
2025-10-05 03:08:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.03 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,38 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
+        int count = 0;
         int n = maps.length;
         int m = maps[0].length;
-        
         boolean[][] visited = new boolean[n][m];
+        int[][] dist = new int[n][m];
         
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
         
+        //큐 초기화
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0,0});
+        queue.add(new int[]{0,0});
         visited[0][0] = true;
+        dist[0][0] = 1;
         
         while(!queue.isEmpty()){
             int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
+            int cx = current[0];
+            int cy = current[1];
             
-            if(x == n-1 && y == m-1){
-                return maps[x][y];
-            }
-            
-            for(int i =0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            for(int i = 0; i<4; i++){
+                int x = cx + dx[i];
+                int y = cy + dy[i];
                 
-                if(nx >= 0 && ny >= 0 && nx <n && ny <m && maps[nx][ny] == 1 && !visited[nx][ny]) {
-                    queue.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                    maps[nx][ny] = maps[x][y]+1;
+                if(x>=0 && y>=0 && x<n && y<m && !visited[x][y] && maps[x][y] == 1){
+                    queue.add(new int[]{x, y});
+                    visited[x][y] = true;
+                    dist[x][y] = dist[cx][cy] + 1;
                 }
             }
         }
-        
-        
-    
-        return -1;
+        return dist[n-1][m-1] == 0 ? -1 : dist[n-1][m-1];
     }
     
 }
