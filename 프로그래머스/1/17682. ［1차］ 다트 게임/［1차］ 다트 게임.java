829f23e2c438929d6869d4dfca4146ec04c8a105
JIWON IN <jr0107380@gmail.com>
2025-10-09 19:04:54 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.11 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,13 +1,13 @@
 import java.util.*;
 class Solution {
     public int solution(String dartResult) {
-        int answer = 0;
         char[] arr = dartResult.toCharArray();
         int[] score = new int[3];
         
         int idx = 0;
         int n = 0;
         for(int i = 0; i<arr.length; i++){
+            //점수 계산
             if(Character.isDigit(arr[i])){
                 //10 점수인 경우
                 if(arr[i] == '1' && arr[i+1] == '0'){
@@ -20,21 +20,9 @@ class Solution {
             }
             
             //보너스 계산 후 배열에 저장
-            else if(arr[i] == 'S'){
-                score[idx] = n;
-                idx++;
-            }
-            else if(arr[i] == 'D') {
-                n = (int)Math.pow(n, 2);
-                score[idx] = n;
-                idx++;
-            }
-            else if(arr[i] == 'T') {
-                n = (int)Math.pow(n, 3);
-                score[idx] = n;
-                idx++;
-            }
-
+            else if(arr[i] == 'S') score[idx++] = n;
+            else if(arr[i] == 'D') score[idx++] = n*n;
+            else if(arr[i] == 'T') score[idx++] = n*n*n;
             
             //옵션 계산
             else if(arr[i] == '*') {
