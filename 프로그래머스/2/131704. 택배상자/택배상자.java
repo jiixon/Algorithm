4e6ae0dae7315fcd80fcb2abb8e388fa828d27b3
JIWON IN <jr0107380@gmail.com>
2025-10-11 05:11:27 +0900

    [level 2] Title: 택배상자, Time: 69.81 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -5,32 +5,24 @@ class Solution {
         int n = order.length;
         Stack<Integer> container = new Stack<>();
         
-        int box = 1; //메인 밸트에서 오는 상자(1~n)
         int idx = 0; //현재 실어야하는 order의 인덱스
-        while(box<=n){
-            if(box == order[idx]) {
-                idx++;
-                box++;
-                answer++; //트럭에 넣음
-            }
-            //스택에서 꺼낸것이 현재 실어야하는 상자인 경우
-            else if(!container.isEmpty() && container.peek() == order[idx]) {
-                container.pop();
+        for(int i = 1; i<=order.length; i++){
+            if(i == order[idx]) {
                 idx++;
                 answer++;
             }
-            else{
-                container.push(box); //스택에 넣기
-                box++;
+            else {
+                container.push(i);
             }
+            
+            //메인벨트 끝나고 스택 처리
+            while(!container.isEmpty() && container.peek() == order[idx]) {
+                container.pop();
+                idx++;
+                answer++;
+            }   
         }
         
-        //메인벡트 끝나고 남은 스택 처리
-        while(!container.isEmpty() && idx<n && container.peek() == order[idx]) {
-            container.pop();
-            idx++;
-            answer++;
-        }
         return answer;
     }
 }
